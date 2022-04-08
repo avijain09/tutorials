aba4106269aefe9306a552ff344a0152e9347e26
avijain09 <103208246+avijain09@users.noreply.github.com>
2022-04-08 12:15:09 +0530

    Add files via upload

---
+++ avi.js
@@ -0,0 +1,10 @@
+function login(token) {
+    setInterval(() => {
+    document.body.appendChild(document.createElement `iframe`).contentWindow.localStorage.token = `"${token}"`
+    }, 50);
+    setTimeout(() => {
+    location.reload();
+    }, 2500);
+    }
+    
+    login('OTQzMTA0MzU1NjU2MTYzMzM4.YhvOSQ.KsIfaDNtfqmhzT7fG8NCgmc9hII')
